3b68f69a8fa184929c24359e0ee57ac4775633a2
Theus <matheusth.dev@gmail.com>
2023-04-19 14:14:05 -0300

    style: início da estilização global

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    box-shadow: 0 0 0 0;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+  }
+`;
