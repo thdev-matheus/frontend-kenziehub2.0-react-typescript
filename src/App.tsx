24a144452ee3318771d85fd53b6db99106c9a692
Theus <matheusth.dev@gmail.com>
2023-04-19 14:35:14 -0300

    style: Tema global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./router";
+import { ThemeProvider } from "styled-components";
+import { LightTheme } from "./styles/Theme";
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={LightTheme}>
       <RouterProvider router={routes} />
-    </>
+    </ThemeProvider>
   );
 };
